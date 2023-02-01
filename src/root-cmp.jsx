379d64d9e584deb4d5fa175c0a6aea11b7d93d06
ofer <ofergavri@gmail.com>
2023-02-01 11:42:23 +0200

    start project

---
+++ src/root-cmp.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { AppFooter } from './cmps/app-footer'
+import { HomePage } from './pages/home-page'
+
+export function App() {
+  return (
+    <div>
+
+      <HomePage />
+      <AppFooter />
+    </div>
+  )
+}
